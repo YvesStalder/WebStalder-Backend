48738168b73346ecaafddde22d8a702ff77cf28f
Yves <yvesstalder2@gmail.com>
2020-04-07 18:19:26 +0200

    feature/master
    --Done--
    Setting up  Project

---
+++ src/main/java/ch/webStalder/backend/WebStalderBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.webStalder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStalderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStalderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/webStalder/backend/WebStalderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.webStalder.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStalderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
